534b14bbab11856b31fd175cfce7c603acc0627d
Richard Reeve <git@richardreeve.net>
2014-09-14 14:24:52 +0100

    Fixed use of community and sub-community in other code.

--- src/CommunityContributions.jl
+++ src/CommunityContributions.jl
@@ -1,7 +1,7 @@
-## contributions() - Calculate diversity contributions from sub-communities
+## contributions() - Calculate diversity contributions from subcommunities
 ##
-## Calculates proportions that sub-communities each contribute to
-## ecosystem diversity per sub-community (perindividual = false), or
+## Calculates proportions that subcommunities each contribute to
+## ecosystem diversity per subcommunity (perindividual = false), or
 ## per individual (perindividual = true) - in the latter case scaled
 ## so that the total # of individuals is 1, since we only have
 ## relative abundances.
@@ -11,21 +11,21 @@
 ## - proportions - population proportions
 ## - qs - single number or vector of values of parameter q
 ## - perindividual - do we measure per individual in population (true)
-##                   or per sub-community (false)
+##                   or per subcommunity (false)
 ## - Z - similarity matrix
 ## - returnecosystem - boolean describing whether to return the
 ##                     ecosystem diversity
-## - returncommunity - boolean describing whether to return the
-##                     community diversities
-## - returnweights - boolean describing whether to return community weights
+## - returnsubcommunity - boolean describing whether to return the
+##                     subcommunity diversities
+## - returnweights - boolean describing whether to return subcommunity weights
 ##
 ## Returns:
-## - contributions of sub-communities to ecosystem diversity (of type measure)
+## - contributions of subcommunities to ecosystem diversity (of type measure)
 ## - and none, some or all (in a tuple) of:
 ##   - vector of ecosystem diversities representing values of q
-##   - array of diversities, first dimension representing sub-communities, and
+##   - array of diversities, first dimension representing subcommunities, and
 ##     last representing values of q
-##   - vector of community weights
+##   - vector of subcommunity weights
 function contributions{S <: FloatingPoint,
                        T <: Number}(measure::Function,
                                     proportions::Matrix{S},
@@ -33,12 +33,12 @@ function contributions{S <: FloatingPoint,
                                     perindividual::Bool = true,
                                     Z::Matrix{S} = eye(size(proportions, 1)),
                                     returnecosystem::Bool = false,
-                                    returncommunity::Bool = false,
+                                    returnsubcommunity::Bool = false,
                                     returnweights::Bool = false)
     ## We need our qs to be a vector of floating points
     powers = 1. - convert(Vector{S}, [qs])
     
-    ## Then calculate the community and ecosystem diversity measures
+    ## Then calculate the subcommunity and ecosystem diversity measures
     ed, cd, w = diversity(measure, proportions, qs, Z, true, true, true)
 
     ## And calculate the contributions
@@ -53,7 +53,7 @@ function contributions{S <: FloatingPoint,
         else
             ## We want to use scaled values so that ultimately we can
             ## get everything relative to total and find out
-            ## contribution per individual or community rather than
+            ## contribution per individual or subcommunity rather than
             ## any kind of actual value
             if (isapprox(power, 0))
                 results[i, :] = w .* log(cd[i, :])
@@ -68,10 +68,10 @@ function contributions{S <: FloatingPoint,
     contrib = perindividual ? (results ./ w) : results
     ## And then return the right bits
     return (returnecosystem ?
-            (returncommunity ?
+            (returnsubcommunity ?
              (returnweights ? (contrib, ed, cd, w) : (contrib, ed, cd)) :
              (returnweights ? (contrib, ed, w) : (contrib, ed))) :
-            (returncommunity ?
+            (returnsubcommunity ?
              (returnweights ? (contrib, cd, w) : (contrib, cd)) :
              (returnweights ? (contrib, w) : (contrib))))
 end
--- src/HistoricalDiversities.jl
+++ src/HistoricalDiversities.jl
@@ -3,7 +3,7 @@ using Diversity
 ## generalisedrichness() - Calculate a generalised version of richness
 ##
 ## Calculates (species) richness of a series of columns representing
-## independent community counts, which is diversity at q = 0 for any
+## independent subcommunity counts, which is diversity at q = 0 for any
 ## diversity measure (passed as the second argument). It also includes
 ## a similarity matrix for the species
 ##
@@ -13,7 +13,7 @@ using Diversity
 ## - Z - similarity matrix
 ##
 ## Returns:
-## - diversity (at ecosystem level) or diversities (of sub-communities)
+## - diversity (at ecosystem level) or diversities (of subcommunities)
 function generalisedrichness{S <: FloatingPoint}(measure::Function,
                                                  proportions::Matrix{S},
                                                  Z::Matrix{S} =
@@ -24,13 +24,13 @@ end
 ## richness() - Calculate species richness of populations
 ##
 ## Calculates (species) richness of a series of columns representing
-## independent community counts, which is diversity at q = 0
+## independent subcommunity counts, which is diversity at q = 0
 ##
 ## Arguments:
 ## - proportions - population proportions
 ##
 ## Returns:
-## - diversities of sub-communities
+## - diversities of subcommunities
 function richness{S <: FloatingPoint}(proportions::Matrix{S})
     generalisedrichness(ᾱ, proportions)
 end
@@ -38,7 +38,7 @@ end
 ## generalisedshannon() - Calculate a generalised version of Shannon entropy
 ##
 ## Calculates Shannon entropy of a series of columns representing
-## independent community counts, which is log(diversity) at q = 1 for
+## independent subcommunity counts, which is log(diversity) at q = 1 for
 ## any diversity measure (passed as the second argument). It also
 ## includes a similarity matrix for the species
 ##
@@ -48,7 +48,7 @@ end
 ## - Z - similarity matrix
 ##
 ## Returns:
-## - entropy (at ecosystem level) or entropies (of sub-communities)
+## - entropy (at ecosystem level) or entropies (of subcommunities)
 function generalisedshannon{S <: FloatingPoint}(measure::Function,
                                                 proportions::Matrix{S},
                                                 Z::Matrix{S} =
@@ -59,13 +59,13 @@ end
 ## shannon() - Calculate shannon entropy of populations
 ##
 ## Calculates shannon entropy of a series of columns representing
-## independent community counts, which is log(diversity) at q = 1
+## independent subcommunity counts, which is log(diversity) at q = 1
 ##
 ## Arguments:
 ## - proportions - population proportions
 ##
 ## Returns:
-## - entropies of sub-communities
+## - entropies of subcommunities
 function shannon{S <: FloatingPoint}(proportions::Matrix{S})
     generalisedshannon(ᾱ, proportions)
 end
@@ -73,7 +73,7 @@ end
 ## generalisedsimpson() - Calculate a generalised version of Simpson's index
 ##
 ## Calculates Simpson's index of a series of columns representing
-## independent community counts, which is 1 / diversity at q = 2 for
+## independent subcommunity counts, which is 1 / diversity at q = 2 for
 ## any diversity measure (passed as the second argument). It also
 ## includes a similarity matrix for the species
 ##
@@ -83,7 +83,7 @@ end
 ## - Z - similarity matrix
 ##
 ## Returns:
-## - concentration (at ecosystem level) or concentrations (of sub-communities)
+## - concentration (at ecosystem level) or concentrations (of subcommunities)
 function generalisedsimpson{S <: FloatingPoint}(measure::Function ,
                                                 proportions::Matrix{S},
                                                 Z::Matrix{S} =
@@ -94,14 +94,14 @@ end
 ## simpson() - Calculate Simpson's index
 ##
 ## Calculates Simpson's index of a series of columns representing
-## independent community counts, which is 1 / diversity (or
+## independent subcommunity counts, which is 1 / diversity (or
 ## concentration) at q = 2
 ##
 ## Arguments:
 ## - proportions - population proportions
 ##
 ## Returns:
-## - concentrations of sub-communities
+## - concentrations of subcommunities
 function simpson{S <: FloatingPoint}(proportions::Matrix{S})
     generalisedsimpson(ᾱ, proportions)
 end
@@ -109,10 +109,10 @@ end
 ## generalisedjaccard() - Calculate a generalised version of Jaccard's index
 ##
 ## Calculates a generalisation of Jaccard's index of a series of
-## columns representing sub-community counts. This evaluates to is G /
+## columns representing subcommunity counts. This evaluates to is G /
 ## A for a series of orders, repesented as a vector of qs (or a single
 ## number). It also includes a similarity matrix for the species. This
-## gives measure of the average distinctiveness of the sub-communities.
+## gives measure of the average distinctiveness of the subcommunities.
 ##
 ## Arguments:
 ## - proportions - population proportions
@@ -124,14 +124,14 @@ end
 function generalisedjaccard(proportions::Matrix, qs,
                             Z::Matrix = eye(size(proportions, 1)))
     size(proportions, 2) == 2 ||
-    error("Can only calculate Jaccard index for 2 communities")
+    error("Can only calculate Jaccard index for 2 subcommunities")
     A(proportions, qs, Z) ./ G(proportions, qs, Z) - 1
 end
 
     ## jaccard() - Calculate Jaccard index
 ##
 ## Calculates Jaccard index (Jaccard similarity coefficient) of two
-## columns representing independent community counts, which is
+## columns representing independent subcommunity counts, which is
 ## A(proportions, 0) / G(proportions, 0) - 1
 ##
 ## Arguments:
