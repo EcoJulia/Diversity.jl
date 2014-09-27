5dc64cdfff61936de85a18f92898f525223abb4b
Richard Reeve <git@richardreeve.net>
2014-09-27 23:32:35 +0100

    Add in documentation for Diversity.Hill package.

--- src/Hill.jl
+++ src/Hill.jl
@@ -1,14 +1,19 @@
 using Diversity
 
-## hillnumber() - calculate Hill number / naive diversity of order q of
-## population(s) with given relative proportions
-##
-## Arguments:
-## - proportions - relative proportions of different individuals / species
-##                 in population (vector, or matrix where columns are
-##                 individual populations) 
-## - qs - single number or vector of orders of diversity measurement
-##
-## Returns:
-## - Diversity of order qs (single number or vector of diversities)
-hillnumber = qD
+@doc """
+### hillnumber()
+
+Calculate the Hill number (or naive diversity) of order q of
+population(s) with given relative proportions
+
+### Arguments:
+* proportions: relative proportions of different individuals / species
+                 in population (vector, or matrix where columns are
+                 individual populations) 
+* qs: single number or vector of orders of diversity measurement
+
+### Returns:
+* Diversity of order qs (single number or vector of diversities)""" ->
+function hillnumber(proportions, qs)
+    qD(proportions, qs)
+end
