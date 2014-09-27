4db4f145fb2b864d1c243f72991caadd845ce24b
Richard Reeve <git@richardreeve.net>
2014-09-27 23:32:04 +0100

    Code to generate documentation in Julia using Lexicon package (must be installed). This is a helper file and not part of the Diversity package.

---
+++ documentation.jl
@@ -0,0 +1,13 @@
+using Lexicon
+using Diversity
+using Diversity.Ecology
+using Diversity.Hill
+using Diversity.Jost
+
+function createdocs(dir::String)
+    slash = (dir[end] == "/") ? "" : "/"
+    save("$(dir)$(slash)diversity.html", Diversity)
+    save("$(dir)$(slash)ecology.html", Diversity.Ecology)
+    save("$(dir)$(slash)hill.html", Diversity.Hill)
+    save("$(dir)$(slash)jost.html", Diversity.Jost)
+end
