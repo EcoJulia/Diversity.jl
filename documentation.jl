2cf74f3ba8a47a0161ce1f2ebb7c409ccd3f17c7
Richard Reeve <git@richardreeve.net>
2014-09-28 22:24:52 +0100

    Fix bug in documentation creation function.

--- documentation.jl
+++ documentation.jl
@@ -5,7 +5,7 @@ using Diversity.Hill
 using Diversity.Jost
 
 function createdocs(dir::String)
-    slash = (dir[end] == "/") ? "" : "/"
+    slash = (dir[end] == '/') ? "" : "/"
     save("$(dir)$(slash)diversity.html", Diversity)
     save("$(dir)$(slash)ecology.html", Diversity.Ecology)
     save("$(dir)$(slash)hill.html", Diversity.Hill)
