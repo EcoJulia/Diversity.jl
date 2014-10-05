a1330ea719f414c6b284c58ee3e141114141890f
Richard Reeve <git@richardreeve.net>
2014-10-05 01:05:18 +0100

    Updated the documentation function to have documentation

--- documentation.jl
+++ documentation.jl
@@ -1,9 +1,23 @@
 using Lexicon
+using Docile
 using Diversity
 using Diversity.Ecology
 using Diversity.Hill
 using Diversity.Jost
+@docstrings
 
+@doc """
+### createdocs()
+
+Create html documentation for Diversity, Diversity.Ecology,
+Diversity.Hill and Diversity.Jost packages in the directory given. The
+master documentation is currently stored in \"doc/site/master\", and
+the stable documentation for the latest released version of the
+packages is stored in \"doc/site/stable\". These will then be uploaded
+to github and displayed on github.io using:
+
+git subtree push --prefix doc/site origin gh-pages
+""" ->
 function createdocs(dir::String)
     slash = (dir[end] == '/') ? "" : "/"
     save("$(dir)$(slash)diversity.html", Diversity)
