83f0397e9535f34547fc8e47791a71532ef491bd
Tony Kelman <tony@kelman.net>
2015-12-26 13:52:37 -0800

    add at-compat for Union{}

--- src/EffectiveNumbers.jl
+++ src/EffectiveNumbers.jl
@@ -47,9 +47,9 @@ powermean{T <: Integer,
                            
 
 ## We need to handle matrices as well as vectors
-function powermean{S <: Number}(values::Union{Vector{S}, Matrix{S}},
+function powermean{S <: Number}(values::@compat(Union{Vector{S}, Matrix{S}}),
                                 orders::Vector,
-                                weights::Union{Vector, Matrix} =
+                                weights::@compat(Union{Vector, Matrix}) =
                                 ones(values) * 1.)
     (size(values) == size(weights)) ||
     error("Values and weights are not the same size")
@@ -71,7 +71,7 @@ population with given relative proportions.
 - Diversity of order qs (single number or vector of diversities)"""
 function qD{S <: AbstractFloat,
             T <: Number}(proportions::Vector{S},
-                         qs::Union{T, Vector{T}})
+                         qs::@compat(Union{T, Vector{T}}))
     if !isapprox(sum(proportions), 1.)
         warn("qD: Population proportions don't sum to 1, fixing...")
         proportions /= sum(proportions)
@@ -81,7 +81,7 @@ end
 
 ## We need to handle matrices as well as vectors
 qD{S <: AbstractFloat,
-   T <: Number}(proportions::Matrix{S}, qs::Union{T, Vector{T}}) =
+   T <: Number}(proportions::Matrix{S}, qs::@compat(Union{T, Vector{T}})) =
        mapslices(p -> qD(p, qs), proportions, 1)
 
 """
@@ -100,7 +100,7 @@ a population with given relative *proportions*, and similarity matrix
 #### Returns:
 - Diversity of order qs (single number or vector of diversities)"""
 function qDZ{S <: AbstractFloat,
-             T <: Number}(proportions::Vector{S}, qs::Union{T, Vector{T}},
+             T <: Number}(proportions::Vector{S}, qs::@compat(Union{T, Vector{T}}),
                           Z::Matrix{S} = eye(length(proportions)))
     if !isapprox(sum(proportions), 1.)
         warn("qDZ: Population proportions don't sum to 1, fixing...")
@@ -115,6 +115,6 @@ end
 
 ## We need to handle matrices as well as vectors
 qDZ{S <: AbstractFloat,
-    T <: Number}(proportions::Matrix{S}, qs::Union{T, Vector{T}},
+    T <: Number}(proportions::Matrix{S}, qs::@compat(Union{T, Vector{T}}),
                  Z::Matrix{S} = eye(size(proportions, 1))) =
                      mapslices(p -> qDZ(p, qs, Z), proportions, 1)
