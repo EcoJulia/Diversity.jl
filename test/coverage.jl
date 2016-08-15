c2cf4a982c10145a6f94ec0414058b9041bba074
Richard Reeve <git@richardreeve.net>
2016-08-15 23:49:22 +0100

    coverage.jl: code tidying.

--- test/coverage.jl
+++ test/coverage.jl
@@ -1,11 +1,12 @@
-# Only run coverage from linux 0.5 build on travis.
-get(ENV, "TRAVIS_OS_NAME", "")       == "linux"   || exit()
-get(ENV, "TRAVIS_JULIA_VERSION", "") == "0.5" || exit()
+# Only run coverage from linux Julia v0.5 build on travis.
+get(ENV, "TRAVIS_OS_NAME", "")       == "linux" || exit()
+get(ENV, "TRAVIS_JULIA_VERSION", "") == "0.5"   || exit()
 
 Pkg.add("Coverage")
 using Coverage
 
 cd(joinpath(dirname(@__FILE__), "..")) do
-    Coveralls.submit(Coveralls.process_folder())
-    Codecov.submit(Codecov.process_folder())
+    processed = process_folder()
+    Coveralls.submit(processed)
+    Codecov.submit(processed)
 end
