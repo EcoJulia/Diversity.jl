8f44a46326d94ce0493330fdf0220debc6816350
Richard Reeve <git@richardreeve.net>
2016-11-30 14:02:22 +0000

    Introduce (raw|norm)(meta|sub)(alpha|beta|rho|gamma) functions to be a shortcut for calculating just one diversity for consistency with boydorr/rdiversity package in R.

--- src/GeneralisedDiversities.jl
+++ src/GeneralisedDiversities.jl
@@ -1,3 +1,46 @@
+function normsubalpha(meta::AbstractMetacommunity, qs)
+    subcommunityDiversity(ᾱ(meta), qs)
+end
+function rawsubalpha(meta::AbstractMetacommunity, qs)
+    subcommunityDiversity(α(meta), qs)
+end
+function normsubbeta(meta::AbstractMetacommunity, qs)
+    subcommunityDiversity(β̄(meta), qs)
+end
+function rawsubbeta(meta::AbstractMetacommunity, qs)
+    subcommunityDiversity(β(meta), qs)
+end
+function normsubrho(meta::AbstractMetacommunity, qs)
+    subcommunityDiversity(ρ̄(meta), qs)
+end
+function rawsubrho(meta::AbstractMetacommunity, qs)
+    subcommunityDiversity(ρ(meta), qs)
+end
+function subgamma(meta::AbstractMetacommunity, qs)
+    subcommunityDiversity(γ(meta), qs)
+end
+function normmetaalpha(meta::AbstractMetacommunity, qs)
+    metacommunityDiversity(ᾱ(meta), qs)
+end
+function rawmetaalpha(meta::AbstractMetacommunity, qs)
+    metacommunityDiversity(α(meta), qs)
+end
+function normmetabeta(meta::AbstractMetacommunity, qs)
+    metacommunityDiversity(β̄(meta), qs)
+end
+function rawmetabeta(meta::AbstractMetacommunity, qs)
+    metacommunityDiversity(β(meta), qs)
+end
+function normmetarho(meta::AbstractMetacommunity, qs)
+    metacommunityDiversity(ρ̄(meta), qs)
+end
+function rawmetarho(meta::AbstractMetacommunity, qs)
+    metacommunityDiversity(ρ(meta), qs)
+end
+function metagamma(meta::AbstractMetacommunity, qs)
+    metacommunityDiversity(γ(meta), qs)
+end
+
 """
 ### Calculates subcommunity and metacommunity diversities
 
