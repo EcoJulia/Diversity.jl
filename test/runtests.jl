9f88d57cdc94a39cafdb62e5f86ba09fb4a9ff35
Richard Reeve <git@richardreeve.net>
2017-04-07 00:13:18 +0100

    Automate testing code so there's no need to specify files.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,40 @@
 using Diversity
 
-tests = ["EffectiveNumbers", "Metacommunity", "DiversityMeasure",
-         "GeneralisedDiversities", "Ecology", "Hill", "Jost"]
+filebase = map(file -> replace(file, r"(.*).jl", s"\1"),
+                filter(file -> ismatch(r".*\.jl", file), readdir("../src")))
+testbase = map(file -> replace(file, r"test_(.*).jl", s"\1"),
+                filter(str -> ismatch(r"test_.*\.jl", str), readdir()))
 
-println("Running tests ...")
+println("Running tests for files:")
+for t in testbase
+    println("    = $t.jl")
+end
+println()
 
-for t in tests
+println("Running tests...")
+for t in testbase
     fn = "test_$t.jl"
-    println("* Testing $t.jl ...")
+    println("    * Testing $t.jl ...")
     include(fn)
+    println()
+end
+
+# Identify tests with no matching file
+superfluous = filter(f -> f ∉ filebase, testbase)
+if length(superfluous) > 0
+    println("Potentially superfluous tests:")
+    for f in superfluous
+        println("    + $f.jl")
+    end
+    println()
+end
+
+# Identify files with no matching test
+missing = filter(f -> f ∉ testbase, filebase)
+if length(missing) > 0
+    println("Potentially missing tests:")
+    for f in missing
+        println("    - $f.jl")
+    end
+    println()
 end
