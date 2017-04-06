d9504ca5f6b68562daf01e89fafc48e74db48851
Richard Reeve <git@richardreeve.net>
2017-04-06 18:59:57 +0100

    Do coverage checking on 0.6.

--- test/coverage.jl
+++ test/coverage.jl
@@ -1,6 +1,6 @@
 # Only run coverage from linux Julia v0.5 build on travis.
-get(ENV, "TRAVIS_OS_NAME", "")       == "linux" || exit()
-get(ENV, "TRAVIS_JULIA_VERSION", "") == "0.5"   || exit()
+get(ENV, "TRAVIS_OS_NAME", "")       == "linux"   || exit()
+get(ENV, "TRAVIS_JULIA_VERSION", "") == "nightly" || exit()
 
 Pkg.add("Coverage")
 using Coverage
