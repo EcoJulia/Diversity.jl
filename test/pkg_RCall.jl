265bec69f325ccb7366d8b0fbd7883898d153de0
Richard Reeve <git@richardreeve.net>
2017-04-09 20:52:20 +0100

    Only do RCall tests on linux.

--- test/pkg_RCall.jl
+++ test/pkg_RCall.jl
@@ -1,12 +1,16 @@
 module ValidateRCall_rdiversity
 using Base.Test
 
+# We can't do the R testing on windows, because I don't know how to install R for appveyor
+@static if is_linux()
+
 using Diversity
 using Diversity.ShortNames
 using RCall
 using DataFrames
 
-R"install.packages('devtools', repos='http://cran.r-project.org', lib=.libPaths()[1])
+R".libPaths("/tmp/R/lib")
+  install.packages('devtools', repos='http://cran.r-project.org', lib="/tmp/R/lib")
   devtools::install_github('boydorr/rdiversity')
   library(rdiversity)"
         
@@ -138,6 +142,8 @@ R"install.packages('devtools', repos='http://cran.r-project.org', lib=.libPaths(
         @test subdiv(ag, qs)[:diversity] ≈ r_asg[:diversity]
 
     end
+end
+
 end
 
 end
