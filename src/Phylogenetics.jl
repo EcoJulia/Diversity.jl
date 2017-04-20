f056e0851494da2b8fcf9ffe0cfdc004bb569749
Richard Reeve <git@richardreeve.net>
2017-04-20 14:54:26 +0100

    Add in first draft of Phylogeny code to generate similarity matrices.

--- src/Phylogenetics.jl
+++ src/Phylogenetics.jl
@@ -1,54 +1,78 @@
 using PhyloTrees
 using Diversity
-import Diversity.counttypes, Diversity.getsimilarity
+import Diversity.counttypes, Diversity.getsimilarity, Diversity.getabundance
 import Diversity.getordinariness, Diversity.getnames
 
 type Phylogeny{Tree <: AbstractTree} <: AbstractTypes
-    num::Int64
-    leafnames::Vector{String}
     tree::Tree
+    nleaf::Int64
+    nancestral::Int64
+    leafnames::Vector{String}
     speciesnames::Vector{String}
     ancestralmatrix::Matrix{Float64}
     Zmatrix::Matrix{Float64}
 
     function (::Type{Phylogeny{Tree}}){Tree <: AbstractTree}(tree::Tree)
         leafnames = getleafnames(tree)
-        num = length(leafnames)
-        num > 0 || error("Too few species")
+        nleaf = length(leafnames)
+        nleaf > 0 || error("Too few species")
         leafinfo = Dict{String, Float64}()
-        speciesinfo = Dict{String, Tuple{String, Float64}}()
+        speciesinfo = Dict{String, Tuple{String, String, Float64}}()
         for leaf in leafnames
             branches = branchpath(tree, leaf)
-            leafinfo[leaf] = getrootdistance(tree, leaf)
+            leafinfo["$leaf"] = getrootdistance(tree, leaf)
             for branch in branches
                 name = "$leaf : $branch"
-                speciesinfo[name] = tuple(leaf, getlength(tree, branch))
+                speciesinfo[name] = tuple("$leaf", "$branch",
+                                          getlength(tree, branch) / leafinfo["$leaf"])
             end
         end
-        
-        new{Tree}(num, leafnames, tree, collect(keys(speciesinfo)),
-                  Matrix{Float64}(), Matrix{Float64}())
+        speciesnames = collect(keys(speciesinfo))
+        nancestral = length(speciesnames)
+        Lbar = mean(values(leafinfo))
+        ancestralmatrix = Matrix{Float64}(nancestral, nleaf)
+        fill!(ancestralmatrix, 0)
+        for i in 1:nancestral
+            for j in 1:nleaf
+                if speciesinfo[speciesnames[i]][1] == leafnames[j]
+                    ancestralmatrix[i, j] =
+                        speciesinfo[speciesnames[i]][3] * leafinfo[leafnames[j]]
+                end
+            end
+        end
+        Zmatrix = Matrix{Float64}(nancestral, nancestral)
+        fill!(Zmatrix, 0)
+        for i in 1:nancestral
+            for j in 1:nancestral
+                if haskey(speciesinfo,
+                          "$(speciesinfo[speciesnames[j]][1]) : $(speciesinfo[speciesnames[i]][2])")
+                    Zmatrix[i, j] = 1.0 / leafinfo[speciesinfo[speciesnames[j]][1]]
+                end
+            end
+        end
+        new{Tree}(tree, nleaf, nancestral, leafnames, speciesnames,
+                  ancestralmatrix, Zmatrix)
     end
 end
 
 Phylogeny{Tree <: AbstractTree}(tree::Tree) = Phylogeny{Tree}(tree)
               
 function counttypes(phy::Phylogeny)
-    return phy.num
+    return phy.nancestral
 end
 
 function getsimilarity(phy::Phylogeny)
-    return eye(phy.num)
+    return phy.Zmatrix * phy.ancestralmatrix
 end
 
-function getordinariness(::Phylogeny, abundances::AbstractArray)
-    return Zmatrix * abundances
+function getordinariness(phy::Phylogeny, abundances::AbstractArray)
+    return phy.Zmatrix * phy.ancestralmatrix * abundances
 end
 
-function getabundance(::Phylogeny, abundances::AbstractArray)
-    return ancestralmatrix * abundances
+function getabundance(phy::Phylogeny, abundances::AbstractArray)
+    return phy.ancestralmatrix * abundances
 end
 
 function getnames(phy::Phylogeny)
-    return phy.leafnames
+    return phy.speciesnames
 end
--- test/test_Phylogenetics.jl
+++ test/test_Phylogenetics.jl
@@ -6,16 +6,17 @@ using Diversity
 using Diversity.Phylogenetics
 
 @testset "PhyloTrees" begin
-    species = ["Dog", "Cat", "Human"]
+    species = ["Dog", "Cat"]#, "Human"]
     nt = NodeTree(species, nodetype=Vector{Float64})
     n = addnode!(nt)
-    addbranch!(nt, n, "Dog", 1.0)
+    addbranch!(nt, n, "Dog", 2.0)
     addbranch!(nt, n, "Cat", 1.0)
     r = addnode!(nt)
-    addbranch!(nt, r, "Human", 2.0)
+#    addbranch!(nt, r, "Human", 2.0)
     addbranch!(nt, r, n, 1.0)
     ph = Phylogeny(nt)
-    @test Set(species) == Set(getnames(ph))
+#    @test Set(species) == Set(getnames(ph))
+#    @test verify(nt)
 end
 
 end
