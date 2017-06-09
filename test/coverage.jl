ba77d3e35fff23752e60ef42f3a7d950b532958f
Richard Reeve <git@richardreeve.net>
2017-06-09 05:15:17 +0100

    Run coverage on 0.6, not nightly.

--- test/coverage.jl
+++ test/coverage.jl
@@ -1,6 +1,6 @@
-# Only run coverage from linux Julia nightly build on travis.
+# Only run coverage from linux Julia 0.6 build on travis.
 get(ENV, "TRAVIS_OS_NAME", "")       == "linux"   || exit()
-get(ENV, "TRAVIS_JULIA_VERSION", "") == "nightly" || exit()
+get(ENV, "TRAVIS_JULIA_VERSION", "") == "0.6" || exit()
 
 Pkg.add("Coverage")
 using Coverage
