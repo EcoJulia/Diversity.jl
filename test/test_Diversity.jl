0ebd886a9748247e626806554f76ef34665322d3
Richard Reeve <git@richardreeve.net>
2018-07-09 22:01:31 +0100

    Remove deprecations with new minor version.

--- test/test_Diversity.jl
+++ test/test_Diversity.jl
@@ -6,22 +6,7 @@ using Diversity
 @testset "Deprecations" begin
     # Only run deprecation checks on macs
     if is_apple()
-        @test_warn "deprecated" subcommunityalpha([1:3...]/6, 1)
-        @test_warn "deprecated" subcommunityalphabar([1:3...]/6, 1)
-        @test_warn "deprecated" subcommunitybeta([1:3...]/6, 1)
-        @test_warn "deprecated" subcommunitybetabar([1:3...]/6, 1)
-        @test_warn "deprecated" subcommunityrho([1:3...]/6, 1)
-        @test_warn "deprecated" subcommunityrhobar([1:3...]/6, 1)
-        @test_warn "deprecated" subcommunitygamma([1:3...]/6, 1)
-        @test_warn "deprecated" subcommunitygammabar([1:3...]/6, 1)
-
-        @test_warn "deprecated" rawmetaalpha([1:3...]/6, 1)
-        @test_warn "deprecated" normmetaalpha([1:3...]/6, 1)
-        @test_warn "deprecated" rawmetabeta([1:3...]/6, 1)
-        @test_warn "deprecated" normmetabeta([1:3...]/6, 1)
-        @test_warn "deprecated" rawmetarho([1:3...]/6, 1)
-        @test_warn "deprecated" normmetarho([1:3...]/6, 1)
-        @test_warn "deprecated" metagamma([1:3...]/6, 1)
+        # No current deprecations?
     end
 end
 
