bbd4cfae1cba6e53a2cc7e471d4e1e802e7cc14e
Richard Reeve <git@richardreeve.net>
2018-08-16 17:15:38 +0100

    Fixing problems in EcoBase for 1.0.

--- ext/EcoBase/src/Interface.jl
+++ ext/EcoBase/src/Interface.jl
@@ -1,3 +1,5 @@
+using Compat
+
 # Functions - most have to be implemented with the concrete type
 occurrences(asm::AbstractAssemblage) = error("function not defined for this type")
 view(asm::AbstractAssemblage) = error("function not defined for this type")
@@ -10,15 +12,19 @@ placenames(plc::AbstractPlaces) = error("function not defined for this type")
 nthings(thg::AbstractThings) = error("function not defined for this type")
 thingnames(thg::AbstractThings) = error("function not defined for this type")
 
-nzrows(a::AbstractMatrix) = find(sum(a .> 0, 2) .> 0)
-nzcols(a::AbstractMatrix) = find(sum(a .> 0, 1) .> 0)
+nzrows(a::AbstractMatrix) = LinearIndices(Compat.sum(a .> 0; dims=2))[findall(Compat.sum(a .> 0; dims=2) .> 0)]
+nzcols(a::AbstractMatrix) = LinearIndices(Compat.sum(a .> 0; dims=1))[findall(Compat.sum(a .> 0; dims=1) .> 0)]
 nnz(a::AbstractArray) = sum(a .> 0)
 
 occurring(asm::AbstractAssemblage) = nzrows(occurrences(asm))
 occupied(asm::AbstractAssemblage) = nzcols(occurrences(asm))
-occupied(asm::AbstractAssemblage, idx) = findn(occurrences(asm)[idx, :])
-occurring(asm::AbstractAssemblage, idx) = findn(occurrences(asm)[:, idx])
-
+if VERSION < v"0.7.0-"
+    occupied(asm::AbstractAssemblage, idx) = collect(zip(findn(occurrences(asm)[idx, :])))
+    occurring(asm::AbstractAssemblage, idx) = collect(zip(findn(occurrences(asm)[:, idx])))
+else
+    occupied(asm::AbstractAssemblage, idx) = findall(!iszero, occurrences(asm)[idx, :])
+    occurring(asm::AbstractAssemblage, idx) = findall(!iszero, occurrences(asm)[:, idx])
+end
 noccurring(x) = length(occurring(x))
 noccupied(x) = length(occupied(x))
 noccurring(x, idx) = length(occurring(x, idx))
@@ -27,11 +33,11 @@ noccupied(x, idx) = length(occupied(x, idx))
 thingoccurrences(asm::AbstractAssemblage, idx) = view(occurrences(asm), idx, :)
 placeoccurrences(asm::AbstractAssemblage, idx) = view(occurrences(asm), :, idx) # make certain that the view implementation also takes thing or place names
 
-richness(asm::AbstractAssemblage{Bool, T, P}) where {T, P} = vec(sum(occurrences(asm), 1))
-richness(asm::AbstractAssemblage) = vec(mapslices(nnz, occurrences(asm), 1))
+richness(asm::AbstractAssemblage{Bool, T, P}) where {T, P} = vec(sum(occurrences(asm), dims=1))
+richness(asm::AbstractAssemblage) = vec(mapslices(nnz, occurrences(asm), dims=1))
 
-occupancy(asm::AbstractAssemblage{Bool, T, P}) where {T, P} = vec(sum(occurrences(asm), 2))
-occupancy(asm::AbstractAssemblage) = vec(mapslices(nnz, occurrences(asm), 2))
+occupancy(asm::AbstractAssemblage{Bool, T, P}) where {T, P} = vec(sum(occurrences(asm), dims=2))
+occupancy(asm::AbstractAssemblage) = vec(mapslices(nnz, occurrences(asm), dims=2))
 
 records(asm::AbstractAssemblage) = nnz(occurrences(asm))
 
@@ -63,18 +69,10 @@ function show(io::IO, asm::T) where T <: AbstractAssemblage
     """)
 end
 
-
-macro forward_func(ex, fs)
-    T, field = ex.args[1], ex.args[2].args[1]
-    T = esc(T)
-    fs = Meta.isexpr(fs, :tuple) ? map(esc, fs.args) : [esc(fs)]
-    :($([:($f(x::$T, args...) = (Base.@_inline_meta; $f($(field)(x), args...)))
-        for f in fs]...);
-    nothing)
-end
-
-@forward_func AbstractAssemblage.places nplaces, placenames
-@forward_func AbstractAssemblage.things nthings, thingnames
+nplaces(asm::AbstractAssemblage, args...) = nplaces(places(asm), args...)
+placenames(asm::AbstractAssemblage, args...) = placenames(places(asm), args...)
+nthings(asm::AbstractAssemblage, args...) = nthings(things(asm), args...)
+thingnames(asm::AbstractAssemblage, args...) = thingnames(things(asm), args...)
 
 # TODO:
 # accessing cache
