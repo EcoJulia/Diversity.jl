df862beff3b4afbbbcf59b7b64dc4761623b5d20
Claire Harris <claire.harris@bioss.ac.uk>
2021-03-01 16:54:24 +0000

    Simplify make file.

--- docs/make.jl
+++ docs/make.jl
@@ -10,6 +10,4 @@ deploydocs(deps = Deps.pip("pygments",
                            "mkdocs",
                            "mkdocs-material",
                            "python-markdown-math"),
-           repo = "github.com/richardreeve/Diversity.jl.git",
-           julia="0.6",
-           osname="linux")
+           repo = "github.com/richardreeve/Diversity.jl.git")
