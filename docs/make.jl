b80cffb179784fc58ee4ddc19427162634d9dd69
Richard Reeve <git@richardreeve.net>
2021-03-04 02:36:45 +0000

    Update docs makefile

--- docs/make.jl
+++ docs/make.jl
@@ -2,12 +2,10 @@ using Documenter
 using Diversity
 
 makedocs(modules = [Diversity,
-                    Diversity.Ecology, Diversity.Jost, Diversity.Hill],
-         clean   = false,
+                    Diversity.Ecology, Diversity.Jost, Diversity.Hill, Diversity.Phylogenetics, Diversity.API],
          sitename = "Diversity.jl")
 
 deploydocs(deps = Deps.pip("pygments",
                            "mkdocs",
                            "mkdocs-material",
-                           "python-markdown-math"),
-           repo = "github.com/richardreeve/Diversity.jl.git")
+                           "python-markdown-math"))
