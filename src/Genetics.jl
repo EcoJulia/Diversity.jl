988de7ae18f709a5ad4edb004b99b8d6daa9756b
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-08-16 17:10:53 +0100

    create genetic type

--- src/Genetics.jl
+++ src/Genetics.jl
@@ -1,44 +1,68 @@
-using Diversity
-using Diversity.API
+# using Diversity
+# using Diversity.API
 
-import PopGen
-import StringDistances
-import LinearAlgebra    # Symmetric()
+# using PopGen
+# using StringDistances
+# using LinearAlgebra
 
-function _hammingDistance(geno1, geno2)
-    ismissing(geno1) || ismissing(geno2) && return missing
-    if length(geno1) > 2
-        @warn "hamming_distance may not work correctly for ploidy > 2"
-    end
-    #TODO Fix ploidy > 2 - e.g. (1, 1, 1, 2) ≠ (1, 2, 2, 2)
+# using FASTX
+
+# abstract type AbstractGeneticTypes{PopData} <: 
+#     Diversity.API.AbstractTypes
+# end
+
+# struct GeneticType{PopData} <: AbstractGeneticTypes{PopData}
+#     dat::PopData
+#     ntypes::Int64
+#     Zmatrix::Matrix{Float64}
+# end
+
+# function _hammingDistance(geno1, geno2)
+#     ismissing(geno1) || ismissing(geno2) && return missing
+#     if length(geno1) > 2
+#         @warn "hamming_distance may not work correctly for ploidy > 2"
+#     end
+#     #TODO Fix ploidy > 2 - e.g. (1, 1, 1, 2) ≠ (1, 2, 2, 2)
     
-    max(sum(geno1 .∉ Ref(geno2)), sum(geno2 .∉ Ref(geno1)))
-end
+#     max(sum(geno1 .∉ Ref(geno2)), sum(geno2 .∉ Ref(geno1)))
+# end
 
-function geneDistance(dat::PopData) 
-    # Initialise objects
-    matrix_obj = PopGen.loci_matrix(dat)
-    N = size(matrix_obj, 1)
-    output = zeros(Float64, N, N)
-    indices = PopGen.pairwise_pairs(1:N)
+# function GeneticType(dat::PopData) 
+#     # Initialise objects
+#     matrix_obj = PopGen.loci_matrix(dat)
+#     ntypes = size(matrix_obj, 1)
+#     output = zeros(Float64, ntypes, ntypes)
+#     indices = PopGen.pairwise_pairs(1:ntypes)
 
-    # Calculate distance matrix
-    for (a, b) in indices
-        output[a, b] = sum(_hammingDistance.((@view matrix_obj[a, :]),
-                                             (@view matrix_obj[b, :])))
-    end
-    return LinearAlgebra.Symmetric(output)
-end
+#     # Calculate distance matrix
+#     for (a, b) in indices
+#         output[a, b] = sum(_hammingDistance.((@view matrix_obj[a, :]),
+#                                              (@view matrix_obj[b, :])))
+#     end
+#     dist = Symmetric(output)
+#     dist /= maximum(dist)
 
-function geneDistance(dat::AbstractVector) 
-    # Initialise objects
-    N = length(dat)
-    output = zeros(Int64, N, N)
-    indices = PopGen.pairwise_pairs(1:N)
+#     # Calculate similarity matrix
+#     Zmatrix = 1 .- dist
 
-    # Calculate distance matrix
-    for (a, b) in indices
-        output[a, b] = StringDistances.evaluate(StringDistances.Hamming(), dat[a][2], dat[b][2])
-    end
-    return LinearAlgebra.Symmetric(output)
-end
+#     return GeneticType{PopData}(dat, ntypes, Zmatrix)
+# end
+
+# function GeneticType(dat::Vector) # Vector{BioSequences.AminoAcidSequence}
+#     # Initialise objects
+#     ntypes = length(dat)
+#     output = zeros(Int64, ntypes, ntypes)
+#     indices = PopGen.pairwise_pairs(1:ntypes)
+
+#     # Calculate distance matrix
+#     for (a, b) in indices
+#         output[a, b] = evaluate(Hamming(), dat[a], dat[b])
+#     end
+#     dist = Symmetric(output)
+#     dist /= maximum(dist)
+
+#     # Calculate similarity matrix
+#     Zmatrix = 1 .- dist
+
+#     return GeneticType{BioSequences.AminoAcidSequence}(dat, ntypes, Zmatrix)
+# end
