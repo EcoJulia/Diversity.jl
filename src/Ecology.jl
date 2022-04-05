7ad72769a3af21208ca7452b7c42ef8d438bf6c4
Isaac Peetoom Heida <isaacheida@gmail.com>
2022-04-04 22:04:31 -0700

    harmonized docsting and documentation notation

--- src/Ecology.jl
+++ src/Ecology.jl
@@ -336,14 +336,14 @@ columns representing independent subcommunity counts.
 
 #### Example:
 ```
-communitydata = [10 20 30 20 0; #5 subcommunities (cols) and 6 species (rows)
+communitymat = [10 20 30 20 0; #5 species (columns) and 6 sites (rows)
                 10 0 50 80 10;
                 60 10 90 0 0; 
                 10 10 10 10 10;
                 70 70 70 70 70;
-                10 0 0 90 0]
+                10 0 0 90 0]'
 
-pielou(communitydata)
+pielou(communitymat)
 ```
 """
 pielou(proportions::AbstractVecOrMat) =
