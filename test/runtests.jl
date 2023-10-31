1fda74d638854627f15652df9ff262c107800940
richardreeve <richard.reeve@glasgow.ac.uk>
2023-10-31 23:46:54 +0000

    Update testing code and workflow, dependencies

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,35 @@
+using Random
 using Test
 
 # Identify files in test/ that are testing matching files in src/
 #  - src/Source.jl will be matched by test/test_Source.jl
-
-filebase = map(file -> replace(file, r"(.*).jl$" => s"\1"),
-                filter(file -> occursin(r".*\.jl$", file),
-                       readdir("../src")))
-testbase = map(file -> replace(file, r"test_(.*).jl$" => s"\1"),
-                filter(str -> occursin(r"^test_.*\.jl$", str), readdir()))
-
-println()
-@info "Running tests for files:"
-for t in testbase
-    println("    = $t.jl")
+filebase = String[]
+for (root, dirs, files) in walkdir("../src")
+    append!(filebase,
+            map(file -> replace(file, r"(.*).jl" => s"\1"),
+                filter(file -> occursin(r".*\.jl", file), files)))
 end
-println()
 
-@testset "* Testing $t.jl" for t in testbase
-    include("test_$t.jl")
+testbase = map(file -> replace(file, r"test_(.*).jl" => s"\1"),
+               filter(str -> occursin(r"^test_.*\.jl$", str), readdir()))
+
+# Seed RNG to make tests reproducible
+Random.seed!(1234)
+
+@testset "Diversity.jl" begin
+    println()
+    @info "Running tests for files:"
+    for t in testbase
+        println("    = $t.jl")
+    end
+    println()
+
+    @info "Running tests..."
+    @testset for t in testbase
+        fn = "test_$t.jl"
+        println("    * Testing $t.jl ...")
+        include(fn)
+    end
 end
 
 # Identify tests with no matching file
@@ -32,11 +44,11 @@ if length(superfluous) > 0
 end
 
 # Identify files with no matching test
-missing = filter(f -> f ∉ testbase, filebase)
-if length(missing) > 0
+notest = filter(f -> f ∉ testbase, filebase)
+if length(notest) > 0
     println()
     @info "Potentially missing tests:"
-    for f in missing
+    for f in notest
         println("    - $f.jl")
     end
     println()
@@ -50,13 +62,17 @@ pkgbase = map(file -> replace(file, r"pkg_(.*).jl$" => s"\1"),
                           readdir()))
 
 if length(pkgbase) > 0
-    @info "Running cross-validation against:"
-    for p in pkgbase
-        println("    = $p")
-    end
-    println()
+    @info "Cross validation packages:"
+    @testset begin
+        for p in pkgbase
+            println("    = $p")
+        end
+        println()
 
-    @testset " * Validating against $p" for p in pkgbase
-        include("pkg_$p.jl")
+        @testset for p in pkgbase
+            fn = "pkg_$p.jl"
+            println("    * Validating $p.jl ...")
+            include(fn)
+        end
     end
 end
