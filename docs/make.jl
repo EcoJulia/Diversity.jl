d7e557ff0294f5fc51db6d462b58e633ab1b1d19
richardreeve <richard.reeve@glasgow.ac.uk>
2024-04-15 02:20:28 +0100

    Update doc code

--- docs/make.jl
+++ docs/make.jl
@@ -3,10 +3,12 @@ using Diversity
 using Phylo
 
 makedocs(modules = [Diversity,
-                    Diversity.Ecology, Diversity.Jost,
-                    Diversity.Hill,
-                    Diversity.ShortNames, Diversity.API],
+             Diversity.Ecology, Diversity.Jost,
+             Diversity.Hill,
+             Diversity.ShortNames, Diversity.API],
          sitename = "Diversity.jl")
 
 deploydocs(repo = "github.com/EcoJulia/Diversity.jl.git",
-           devbranch = "dev")
+           push_preview = true,
+           devbranch = "dev",
+           devurl = "dev")
