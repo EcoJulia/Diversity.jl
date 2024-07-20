fd926b5721fdddadf2a9f47e1dc3bbfe3e0f6a88
richardreeve <richard.reeve@glasgow.ac.uk>
2024-07-20 23:37:36 +0100

    Using ResearchSoftwareMetadata package

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,11 @@
 # SPDX-License-Identifier: BSD-2-Clause
 
+using Pkg
+Pkg.update()
+"Diversity" ∈ [p.name for p in values(Pkg.dependencies())] &&
+    Pkg.rm("Diversity")
+Pkg.develop("Diversity")
+
 using Documenter
 using Diversity
 using Phylo
